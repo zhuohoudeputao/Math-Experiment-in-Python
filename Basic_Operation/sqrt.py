2b217de62194becf64ea5238c4707df238723330
zhuohoudeputao <linzuxuan@outlook.com>
2020-06-21 00:27:41 +0800

    update arrangement

---
+++ Basic_Operation/sqrt.py
@@ -0,0 +1,7 @@
+'''
+@Author: your name
+@LastEditTime: 2020-06-21 00:18:12
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /Math-Experiment-in-Python/Basic_Operation/sqrt.py
+'''
---
+++ Basic_Operation/subplot.py
@@ -0,0 +1,7 @@
+'''
+@Author: zhuohoudeputao
+@LastEditors: zhuohoudeputao
+@LastEditTime: 2020-06-21 00:24:51
+@Description: file content
+'''
+
