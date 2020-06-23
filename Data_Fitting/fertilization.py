fc3bbd4b28526f5e1a37899091ee2e348c2dd2cf
zhuohoudeputao <linzuxuan@outlook.com>
2020-06-24 01:37:59 +0800

    update arrangement

---
+++ Data_Fitting/fertilization.py
@@ -0,0 +1,6 @@
+'''
+@Author: zhuohoudeputao
+@LastEditors: zhuohoudeputao
+@LastEditTime: 2020-06-24 01:36:36
+@Description: file content
+'''
---
+++ Data_Fitting/numerical_series.py
@@ -0,0 +1,6 @@
+'''
+@Author: zhuohoudeputao
+@LastEditors: zhuohoudeputao
+@LastEditTime: 2020-06-24 01:35:18
+@Description: file content
+'''
